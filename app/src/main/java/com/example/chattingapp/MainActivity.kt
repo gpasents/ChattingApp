0b77b0fa179805d05e6fc9e0dd8b8b6dba002d13
Giorgos <gpasents@gmail.com>
2021-05-12 13:07:46 +0300

    add Register and Login activities

--- app/src/main/java/com/example/chattingapp/MainActivity.kt
+++ app/src/main/java/com/example/chattingapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.chattingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val name = findViewById<TextView>(R.id.editTextTextPersonName2)
+            val email = findViewById<TextView>(R.id.registerEmail)
+            val password = findViewById<TextView>(R.id.registerPassword)
+
+            Log.d("mainactivity", name.text.toString() + email.text.toString() + password.text.toString())
+
+        }
+
+        val already = findViewById<TextView>(R.id.already)
+        already.setOnClickListener {
+            setContentView(R.layout.activity_login)
+
+            val loginButton = findViewById<Button>(R.id.loginButton)
+            loginButton.setOnClickListener {
+                val login_email = findViewById<TextView>(R.id.loginEmail)
+                val login_password = findViewById<TextView>(R.id.loginPassword)
+
+                Log.d("mainactivity2",  login_email.text.toString() + login_password.text.toString())
+
+            }
+        }
+
+
     }
 }
