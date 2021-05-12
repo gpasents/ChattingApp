e2889cf28d662126af6f7aa112b16a1f6b21417f
Giorgos <gpasents@gmail.com>
2021-05-12 17:55:30 +0300

    add firebase and error handling

--- app/src/main/java/com/example/chattingapp/MainActivity.kt
+++ app/src/main/java/com/example/chattingapp/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,17 @@ class MainActivity : AppCompatActivity() {
             val email = findViewById<TextView>(R.id.registerEmail)
             val password = findViewById<TextView>(R.id.registerPassword)
 
-            Log.d("mainactivity", name.text.toString() + email.text.toString() + password.text.toString())
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.text.toString(),password.text.toString())
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("main","SUccessfuly created user with id"+ it.result?.user?.uid.toString())
+                    Toast.makeText(this, "SUccessfuly created user with id"+ it.result?.user?.uid.toString(),Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "failed to create usr with error: " + it.message.toString(),Toast.LENGTH_SHORT).show()
+                    Log.d("main", "failed to create usr with error: " + it.message.toString())
+                }
 
         }
 
@@ -31,4 +43,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+
 }
